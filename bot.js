057c21b533bc7fce2f18e19a7fa18704c482eaf1
Kenny <keacolli@ucsc.edu>
2020-07-11 13:33:16 -0700

    0.1.2 add creative server status

--- bot.js
+++ bot.js
@@ -5,30 +5,53 @@ const axios = require('axios');
 
 const client = new Discord.Client();
 
+const version = '0.1.2';
+
 client.on('ready', () => {
   console.log('Connected as ' + client.user.tag);
 });
 
 client.on('message', (message) => {
-  if (message.content === '!status') {
-    axios
-      .get('https://mcapi.us/server/status?ip=24.5.218.137')
-      .then(function ({ data }) {
-        let resMsg = '';
-        resMsg += data.online
-          ? ' :monkey:  Server is running on 24.5.218.137'
-          : ':x: Server is Down!';
-        resMsg += '\n';
-        resMsg +=
-          data.players.now > 0
-            ? `:monkey_face:  There are ${data.players.now}/20 players online right now`
-            : ':x:  Nobody is playing';
-        message.channel.send(resMsg);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+  // const statusPrefix = '!status';
+  // if (message.content.startsWith(statusPrefix)) {
+  //   const args = message.content.slice(statusPrefix.length).split(/ +/);
+  //   const command = args.shift().toLowerCase();
+
+  //   if (args.length === 0){
+  //     getStatus('24.5.218.137', '25565', message, ':monkey:', ':monkey_face:');
+  //     getStatus('24.5.218.137', '25566', message, ':soap:', ':poo:');
+  //   } else
+  // }
+
+  switch (message.content) {
+    case '!status':
+      getStatus('24.5.218.137', '25565', message, ':monkey:', ':monkey_face:');
+      getStatus('24.5.218.137', '25566', message, ':soap:', ':poo:');
+      break;
+    case '!version':
+      message.channel.send(version);
+      break;
   }
 });
+
+function getStatus(ip, port, message, runningEmoji, playersEmoji) {
+  axios
+    .get(`https://mcapi.us/server/status?ip=${ip}&port=${port}`)
+    .then(function ({ data }) {
+      let resMsg = '';
+      resMsg += data.online
+        ? `${runningEmoji}  Server is running on ${ip}:${port}`
+        : ':x: Server is Down!';
+      resMsg += '\n';
+      resMsg +=
+        data.players.now > 0
+          ? `${playersEmoji}  There are ${data.players.now}/20 players online right now`
+          : ':x:  Nobody is playing';
+      message.channel.send(resMsg);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
 
 client.login(process.env.DISCORD_TOKEN);
