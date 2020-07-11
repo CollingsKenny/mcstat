f6e801ac00ab40bb0b5769c691b56909bd8e377c
Kenny <keacolli@ucsc.edu>
2020-07-10 17:27:24 -0700

    init mc status bot

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const axios = require('axios');
+
+client.on('ready', () => {
+  console.log('Connected as ' + client.user.tag);
+});
+
+client.on('message', (message) => {
+  if (message.content === '!status') {
+    axios
+      .get('https://mcapi.us/server/status?ip=24.5.218.137')
+      .then(function ({ data }) {
+        //console.log(data);
+        let resMsg = '';
+        resMsg += data.online
+          ? ' :monkey:  Server is running on 24.5.218.137'
+          : ':x: Server is Down!';
+        resMsg += '\n';
+        resMsg +=
+          data.players.now > 0
+            ? `:monkey_face:  There are ${data.players.now}/20 players online right now`
+            : ':x:  Nobody is playing';
+        message.channel.send(resMsg);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+});
+
+client.login('NzMwNjIzNjQ1MDU5MzgzMzI3.XwjTzw.zjZU4UqWfhV13tBFqeHDMLpJ9qs');
